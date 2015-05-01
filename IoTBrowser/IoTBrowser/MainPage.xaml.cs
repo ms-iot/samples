7a4b2238740d3d8ec5b73f6153f9f3827de534a0
David Shoemaker <David.Shoemaker@microsoft.com>
2015-05-01 13:52:59 -0700

    New IoTBrowser sample.

---
+++ IoTBrowser/IoTBrowser/MainPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace IoTBrowser
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Go_Web_Click(object sender, RoutedEventArgs e)
+        {
+            DoWebNavigate();
+        }
+
+
+        private void Web_Address_KeyUp(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+            {
+                DoWebNavigate();
+            }
+        }
+
+        private async void DoWebNavigate()
+        {
+            try
+            {
+                if (Web_Address.Text.Length > 0)
+                {
+                    webView.Navigate(new Uri(Web_Address.Text));
+                }
+                else
+                {
+                    MessageDialog dlg = new MessageDialog("you need to enter a web address.");
+                    await dlg.ShowAsync();
+                }
+            }
+            catch (Exception e)
+            {
+                MessageDialog dlg = new MessageDialog("Error: " + e.Message);
+                await dlg.ShowAsync();
+
+            }
+        }
+
+        private void Go_IoTPortal_Click(object sender, RoutedEventArgs e)
+        {
+            Web_Address.Text = "https://devx.windows-int.com/en-us/iot";
+            DoWebNavigate();
+        }
+
+        private void Go_Hackster_Click(object sender, RoutedEventArgs e)
+        {
+            Web_Address.Text = "https://www.hackster.io/windowsiot";
+            DoWebNavigate();
+        }
+
+        private void Go_GitHub_Click(object sender, RoutedEventArgs e)
+        {
+            Web_Address.Text = "https://github.com/ms-iot";
+            DoWebNavigate();
+        }
+    }
+}
---
+++ IoTBrowser/README.md
@@ -0,0 +1,18 @@
+Windows 10 IoT Browser Sample
+==============
+This sample is a simple WebView control wrapped in a UAP application.
+
+It allows for internet browsing from your Pi2. 
+
+###Requirements:
+<ol>
+<li>USB keyboard</li>
+<li>USB Mouse</li>
+<li>Pi2</li>
+<li>Windows 10 Core installed and running on the Pi2</li>
+</ol>
+
+###Usage
+Build your own, you can open the IoTBrowser solution in Visual Studio 2015 Community Edition and build the code then deploy and debug on your device.
+
+Or there is a released package at: https://github.com/DavidShoe/samples/releases/tag/v1.0.3-alpha
