2fee098973ba50453f446f3c5f4ac1838344aca6
Jordan Rhee <jordanrh@jordanrh5331hp.redmond.corp.microsoft.com>
2015-05-07 14:06:23 -0700

    Initial updates to I2CAccelerometer sample (not tested).

--- I2CAccelerometer/CS/MainPage.xaml.cs
+++ I2CAccelerometer/CS/MainPage.xaml.cs
@@ -26,6 +26,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -40,15 +41,18 @@ using Windows.Devices.I2c;
 
 namespace I2CAccelerometer
 {
+    struct Acceleration
+    {
+        double X;
+        double Y;
+        double Z;
+    };
+    
     /// <summary>
     /// Sample app that reads data over I2C from an attached ADXL345 accelerometer
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        /* Important! Set the correct I2C controller name for your target device here */
-        private const string I2C_CONTROLLER_NAME = "I2C5";        /* For Minnowboard Max, use I2C5 */
-        //private const string I2C_CONTROLLER_NAME = "I2C1";        /* For Raspberry Pi 2, use I2C1 */
-
         private const byte ACCEL_I2C_ADDR = 0x53;           /* 7-bit I2C address of the ADXL345      */
         private const byte ACCEL_REG_POWER_CONTROL = 0x2D;  /* Address of the Power Control register */
         private const byte ACCEL_REG_DATA_FORMAT = 0x31;    /* Address of the Data Format register   */
@@ -57,7 +61,7 @@ namespace I2CAccelerometer
         private const byte ACCEL_REG_Z = 0x36;              /* Address of the Z Axis data register   */
 
         private I2cDevice I2CAccel;
-        private DispatcherTimer periodicTimer;
+        private Timer periodicTimer;
 
         public MainPage()
         {
@@ -72,20 +76,24 @@ namespace I2CAccelerometer
 
         private async void InitI2CAccel()
         {
-            /* Initialize the I2C bus */
-            try {
-                var settings = new I2cConnectionSettings(ACCEL_I2C_ADDR); 
-                settings.BusSpeed = I2cBusSpeed.FastMode;
-
-                string aqs = I2cDevice.GetDeviceSelector(I2C_CONTROLLER_NAME);  /* Find the selector string for the I2C bus controller                   */
-                var dis = await DeviceInformation.FindAllAsync(aqs);            /* Find the I2C bus controller device with our selector string           */
-                I2CAccel = await I2cDevice.FromIdAsync(dis[0].Id, settings);    /* Create an I2cDevice with our selected bus controller and I2C settings */
-
-            }
-            /* If initialization fails, display the exception and stop running */
-            catch (Exception e)
+            string aqs = I2cDevice.GetDeviceSelector();                     /* Get a selector string that will return all I2C controllers on the system */
+            var dis = await DeviceInformation.FindAllAsync(aqs);            /* Find the I2C bus controller device with our selector string           */
+            if (dis.Count == 0)
             {
-                Text_Status.Text = "Exception: " + e.Message;
+                Text_Status.Text = "No I2C controllers were found on the system";
+                return;
+            }
+            
+            var settings = new I2cConnectionSettings(ACCEL_I2C_ADDR); 
+            settings.BusSpeed = I2cBusSpeed.FastMode;
+            I2CAccel = await I2cDevice.FromIdAsync(dis[0].Id, settings);    /* Create an I2cDevice with our selected bus controller and I2C settings */
+            if (I2CAccel == null)
+            {
+                Text_Status.Text = string.Format(
+                    "Slave address {0} on I2C Controller {1} is currently in use by " +
+                    "another application. Please ensure that no other applications are using I2C.",
+                    settings.SlaveAddres,
+                    dis[0].Id);
                 return;
             }
 
@@ -113,10 +121,7 @@ namespace I2CAccelerometer
             }
 
             /* Now that everything is initialized, create a timer so we read data every 100mS */
-            periodicTimer = new DispatcherTimer();
-            periodicTimer.Interval = TimeSpan.FromMilliseconds(100);
-            periodicTimer.Tick += Timer_Tick;
-            periodicTimer.Start();
+            periodicTimer = new Timer(this.TimerCallback, null, 0, 100);
         }
 
         private void MainPage_Unloaded(object sender, object args)
@@ -124,62 +129,70 @@ namespace I2CAccelerometer
             /* Cleanup */
             I2CAccel.Dispose();
         }
-
-        private void ReadI2CAccel()
+        
+        private void TimerCallback(object state)
         {
-            const string FLOAT_FORMAT = "F3";   /* Specify that we want 3 decimal points for floating point numbers                */
+            string xText, yText, zText;
+            string statusText;
+            
+            try
+            {
+                Acceleration accel = ReadI2CAccel();
+                xText = String.Format("X Axis: {0:F3}G", accel.X);
+                yText = String.Format("Y Axis: {0:F3}G", accel.Y);
+                zText = String.Format("Z Axis: {0:F3}G", accel.Z);
+                statusText = "Status: Running";
+            }
+            catch (Exception ex)
+            {
+                xText = "X Axis: Error";
+                yText = "Y Axis: Error";
+                zText = "Z Axis: Error";
+                statusText = "Failed to read from Accelerometer: " + ex.Message;
+            }
+            
+            // UI updates must be invoked on the UI thread.
+            var task = this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                Text_X_Axis.Text = xText;
+                Text_Y_Axis.Text = yText;
+                Text_Z_Axis.Text = zText;
+                Text_Status.Text = statusText;
+            });
+        }
+
+        private Acceleration ReadI2CAccel()
+        {
             const int ACCEL_RES = 1024;         /* The ADXL345 has 10 bit resolution giving 1024 unique values                     */
             const int ACCEL_DYN_RANGE_G = 8;    /* The ADXL345 had a total dynamic range of 8G, since we're configuring it to +-4G */
             const int UNITS_PER_G = ACCEL_RES / ACCEL_DYN_RANGE_G;  /* Ratio of raw int values to G units                          */
      
             Int16 AccelerationRawX, AccelerationRawY, AccelerationRawZ; /* Raw readings from the accelerometer */
-            double AccelerationX, AccelerationY, AccelerationZ;         /* Readings converted to G units       */
 
             byte[] RegAddrBuf = new byte[] { ACCEL_REG_X }; /* Register address we want to read from                                         */
             byte[] ReadBuf = new byte[6];                   /* We read 6 bytes sequentially to get all 3 two-byte axes registers in one read */
-
-            try
-            {
-                /* 
-                 * Read from the accelerometer 
-                 * We call WriteRead() so we first write the address of the X-Axis I2C register, then read all 3 axes
-                 */
-                I2CAccel.WriteRead(RegAddrBuf, ReadBuf);
-            }
-            catch (Exception e)
-            {
-                /* If WriteRead() fails, display error messages */
-                Text_X_Axis.Text = "X Axis: Error";
-                Text_Y_Axis.Text = "Y Axis: Error";
-                Text_Z_Axis.Text = "Z Axis: Error";
-                Text_Status.Text = "Exception: " + e.Message;
-                return;
-            }
+            
+            /* 
+             * Read from the accelerometer 
+             * We call WriteRead() so we first write the address of the X-Axis I2C register, then read all 3 axes
+             */
+            I2CAccel.WriteRead(RegAddrBuf, ReadBuf);        
 
             /* 
              * In order to get the raw 16-bit data values, we need to concatenate two 8-bit bytes from the I2C read for each axis.
              * We accomplish this by using bit shift and logical OR operations
              */
-            AccelerationRawX = (Int16)(ReadBuf[0] | ReadBuf[1] << 8);
-            AccelerationRawY = (Int16)(ReadBuf[2] | ReadBuf[3] << 8);
-            AccelerationRawZ = (Int16)(ReadBuf[4] | ReadBuf[5] << 8);
+            AccelerationRawX = BitConverter.ToInt16(ReadBuf, 0);
+            AccelerationRawY = BitConverter.ToInt16(ReadBuf, 2);
+            AccelerationRawZ = BitConverter.ToInt16(ReadBuf, 4);
 
             /* Convert raw values to G's */
-            AccelerationX = (double)AccelerationRawX / UNITS_PER_G;
-            AccelerationY = (double)AccelerationRawY / UNITS_PER_G;
-            AccelerationZ = (double)AccelerationRawZ / UNITS_PER_G;
+            Acceleration accel;
+            accel.X = (double)AccelerationRawX / UNITS_PER_G;
+            accel.Y = (double)AccelerationRawY / UNITS_PER_G;
+            accel.Z = (double)AccelerationRawZ / UNITS_PER_G;
 
-            /* Display the values */
-            Text_X_Axis.Text = "X Axis: " + AccelerationX.ToString(FLOAT_FORMAT) + "G";
-            Text_Y_Axis.Text = "Y Axis: " + AccelerationY.ToString(FLOAT_FORMAT) + "G";
-            Text_Z_Axis.Text = "Z Axis: " + AccelerationZ.ToString(FLOAT_FORMAT) + "G";
-            Text_Status.Text = "Status: Running";
+            return accel;
         }
-
-        private void Timer_Tick(object sender, object e)
-        {
-            ReadI2CAccel(); /* Read data from the I2C accelerometer and display it */
-        }
-
     }
 }
