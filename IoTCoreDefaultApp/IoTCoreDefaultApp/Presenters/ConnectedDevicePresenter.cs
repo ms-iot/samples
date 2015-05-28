ed36ac64b6b0b7db2d98683fea783c3b894cde22
Rachit Bajpai <rachitb@microsoft.com>
2015-05-28 14:07:03 -0700

    Picking up Latest Bertha Changes from SD

---
+++ IoTCoreDefaultApp/IoTCoreDefaultApp/Presenters/ConnectedDevicePresenter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.UI.Core;
+
+namespace IoTCoreDefaultApp
+{
+    public class ConnectedDevicePresenter
+    {
+        private CoreDispatcher dispatcher;
+
+        public ConnectedDevicePresenter(CoreDispatcher dispatcher)
+        {
+            this.dispatcher = dispatcher;
+            var deviceClass = "(System.Devices.InterfaceClassGuid:=\"{" + Constants.GUID_DEVINTERFACE_USB_DEVICE + "}\")";
+
+            usbConnectedDevicesWatcher = DeviceInformation.CreateWatcher(deviceClass);
+            usbConnectedDevicesWatcher.Added += DevicesAdded;
+            usbConnectedDevicesWatcher.Start();
+        }
+
+        private async void DevicesAdded(DeviceWatcher sender, DeviceInformation args)
+        {
+            await dispatcher.RunAsync(CoreDispatcherPriority.Low, () =>
+            {
+                devices.Add(args.Name);
+            });
+        }
+
+        public ObservableCollection<string> GetConnectedDevices()
+        {
+            return devices;
+        }
+
+        private ObservableCollection<string> devices = new ObservableCollection<string>();
+        private DeviceWatcher usbConnectedDevicesWatcher;
+    }
+}
---
+++ IoTCoreDefaultApp/IoTCoreDefaultApp/Views/TutorialMainPage.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace IoTCoreDefaultApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class TutorialMainPage : Page
+    {
+        private DispatcherTimer timer;
+
+        public TutorialMainPage()
+        {
+            this.InitializeComponent();
+#if !RPI
+            TutorialList.Items.Remove(HelloBlinkyGridViewItem);
+#endif
+
+            UpdateDateTime();
+
+            timer = new DispatcherTimer();
+            timer.Tick += timer_Tick;
+            timer.Interval = TimeSpan.FromSeconds(30);
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, object e)
+        {
+            UpdateDateTime();
+        }
+
+        private void UpdateDateTime()
+        {
+            var t = DateTime.Now;
+            this.CurrentTime.Text = t.ToString("t", CultureInfo.CurrentCulture);
+        }
+
+        private void ShutdownButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            ShutdownDropdown.IsOpen = true;
+        }
+
+        private void ShutdownHelper(ShutdownKind kind)
+        {
+            ShutdownManager.BeginShutdown(kind, TimeSpan.FromSeconds(0.5));
+        }
+
+        private void ShutdownListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var item = e.ClickedItem as FrameworkElement;
+            if (item == null)
+            {
+                return;
+            }
+            switch (item.Name)
+            {
+                case "ShutdownOption":
+                    ShutdownHelper(ShutdownKind.Shutdown);
+                    break;
+                case "RestartOption":
+                    ShutdownHelper(ShutdownKind.Restart);
+                    break;
+            }
+        }
+
+        private void SettingsButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            NavigationUtils.NavigateToScreen(typeof(Settings));
+        }
+
+        private void DeviceInfo_Clicked(object sender, RoutedEventArgs e)
+        {
+            NavigationUtils.NavigateToScreen(typeof(MainPage));
+        }
+
+        private void TutorialList_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var item = e.ClickedItem as FrameworkElement;
+            if (item == null)
+            {
+                return;
+            }
+            switch (item.Name)
+            {
+                case "HelloBlinky":
+                    NavigationUtils.NavigateToScreen(typeof(TutorialHelloBlinkyPage), item.Name);
+                    break;
+                default:
+                    NavigationUtils.NavigateToScreen(typeof(TutorialContentPage), item.Name);
+                    break;
+            }
+        }
+    }
+}
