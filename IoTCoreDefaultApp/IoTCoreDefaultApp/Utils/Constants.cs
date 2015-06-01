1d9aa6450c32b4471f2c88d050ccc92a4716b180
Muhamad Muhamad Lotfy <mlotfy@microsoft.com>
2015-06-01 11:15:36 -0700

    Support dynamic USB devices enumeration
    - Fix a bug where connected USB devices list does not get updated when a
    previously enumerated device gets removed or add
    - Fix a bug where a previously enumerated device is considered connected
    even if it is not present
    - Add a couple of debug prints to ease debugging

--- IoTCoreDefaultApp/IoTCoreDefaultApp/Utils/Constants.cs
+++ IoTCoreDefaultApp/IoTCoreDefaultApp/Utils/Constants.cs
@@ -28,7 +28,7 @@ namespace IoTCoreDefaultApp
     {
         public static string HasDoneOOBEKey = "DefaultAppHasDoneOOBE";
         public static string HasDoneOOBEValue = "YES";
-        public static string GUID_DEVINTERFACE_USB_DEVICE = "A5DCBF10-6530-11D2-901F-00C04FB951ED";
+        public const string GUID_DEVINTERFACE_USB_DEVICE = "A5DCBF10-6530-11D2-901F-00C04FB951ED";
         public static string[] TutorialDocNames = {
             "GetStarted",
 #if RPI
