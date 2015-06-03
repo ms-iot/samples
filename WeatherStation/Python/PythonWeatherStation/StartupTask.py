489aa9192facb553ad3f47f28b0f8aa1a35cff5e
jessekaplan <jessekaplan@users.noreply.github.com>
2015-06-03 16:50:14 -0700

    Adding python weather station Fixing a bug in App2App sample

---
+++ WeatherStation/Python/PythonWeatherStation/StartupTask.py
@@ -0,0 +1,24 @@
+import _wini2c as i2c
+import time
+
+sample_temp_hold = bytes([0xe3])
+sample_humidity_hold = bytes([0xe5])
+
+htdu21d = i2c.i2cdevice('I2C1', 0x40, i2c.FASTSPEED, i2c.SHAREDMODE)
+gpio.setup(5,gpio.OUT)
+
+while True:
+    temp_data = htdu21d.writeread(sample_temp_hold, 3)
+    tempReading = temp_data[0] << 8 | temp_data[1]
+    tempRatio = tempReading/65536.0
+    temperature = (-46.85 + (175.72 * tempRatio)) * 9 / 5 + 32
+
+    print("Temperature: ",temperature)
+
+    humidity_data = htdu21d.writeread(sample_humidity_hold, 3)
+    humidityReading = humidity_data[0] << 8 | humidity_data[1]
+    humidityRatio = humidityReading / 65536.0
+    humidity = -6 + (125 * humidityRatio)
+    print("Humidity: " , humidity)
+
+    time.sleep(1)
