5acb7f0a54ea68fe914d003c227ea06675450cdb
jessekaplan <jessekaplan@users.noreply.github.com>
2015-06-11 14:32:11 -0700

    Updating copyright

--- WeatherStation/Python/PythonWeatherStation/StartupTask.py
+++ WeatherStation/Python/PythonWeatherStation/StartupTask.py
@@ -1,35 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> de59284... Fixing publisher names and adding copyright message
-"""
-Copyright(c) Microsoft Open Technologies, Inc.All rights reserved.
+""" Copyright (c) Microsoft. All rights reserved."""
 
-   The MIT License(MIT)
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files(the "Software"), to deal 
-    in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject tothe following conditions :
-
-    The above copyright notice and this permission notice shall be included in
-    all copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-"""
-<<<<<<< HEAD
-=======
->>>>>>> c992184... Adding python weather station
-=======
->>>>>>> de59284... Fixing publisher names and adding copyright message
 import _wini2c as i2c
 import time
 
@@ -37,13 +7,6 @@ sample_temp_hold = bytes([0xe3])
 sample_humidity_hold = bytes([0xe5])
 
 htdu21d = i2c.i2cdevice('I2C1', 0x40, i2c.FASTSPEED, i2c.SHAREDMODE)
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-gpio.setup(5,gpio.OUT)
->>>>>>> c992184... Adding python weather station
-=======
->>>>>>> d351786... Fixes python samples, adding license text and Microsoft as publisher for others
 
 while True:
     temp_data = htdu21d.writeread(sample_temp_hold, 3)
