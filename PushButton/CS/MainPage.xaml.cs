823f373c958c2db7675a19aee26d4baf570b75e7
Daniel Kuo <dankuotest@outlook.com>
2015-06-18 13:57:01 -0700

    Updating sample to support both MBM and RPi2
    Changed button and LED pin numbers and other minor fixes

--- PushButton/CS/MainPage.xaml.cs
+++ PushButton/CS/MainPage.xaml.cs
@@ -53,14 +53,22 @@ namespace PushButton
             buttonPin = gpio.OpenPin(BUTTON_PIN);
             ledPin = gpio.OpenPin(LED_PIN);
 
-            ledPin.Write(GpioPinValue.Low);
+            // Initialize LED to the OFF state by first writing a HIGH value
+            // We write HIGH because the LED is wired in a active LOW configuration
+            ledPin.Write(GpioPinValue.High); 
             ledPin.SetDriveMode(GpioPinDriveMode.Output);
 
+            // Check if input pull-up resistors are supported
             if (buttonPin.IsDriveModeSupported(GpioPinDriveMode.InputPullUp))
                 buttonPin.SetDriveMode(GpioPinDriveMode.InputPullUp);
             else
                 buttonPin.SetDriveMode(GpioPinDriveMode.Input);
+
+            // Set a debounce timeout to filter out switch bounce noise from a button press
             buttonPin.DebounceTimeout = TimeSpan.FromMilliseconds(50);
+
+            // Register for the ValueChanged event so our buttonPin_ValueChanged 
+            // function is called when the button is pressed
             buttonPin.ValueChanged += buttonPin_ValueChanged;
 
             GpioStatus.Text = "GPIO pins initialized correctly.";
@@ -68,12 +76,12 @@ namespace PushButton
 
         private void buttonPin_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs e)
         {
-            // toggle the state of the onboard LED every time the button is pressed
+            // toggle the state of the LED every time the button is pressed
             if (e.Edge == GpioPinEdge.FallingEdge)
             {
-                ledPin.Write(ledPinValue);
                 ledPinValue = (ledPinValue == GpioPinValue.Low) ?
                     GpioPinValue.High : GpioPinValue.Low;
+                ledPin.Write(ledPinValue);
             }
 
             // need to invoke UI updates on the UI thread because this event
@@ -81,22 +89,22 @@ namespace PushButton
             var task = Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => {
                 if (e.Edge == GpioPinEdge.FallingEdge)
                 {
-                    ledEllipse.Fill = redBrush;
+                    ledEllipse.Fill = (ledPinValue == GpioPinValue.Low) ? 
+                        redBrush : grayBrush;
                     GpioStatus.Text = "Button Pressed";
                 }
                 else
                 {
-                    ledEllipse.Fill = grayBrush;
                     GpioStatus.Text = "Button Released";
                 }
             });
         }
 
-        private const int LED_PIN = 47;
+        private const int LED_PIN = 6;
         private const int BUTTON_PIN = 5;
         private GpioPin ledPin;
         private GpioPin buttonPin;
-        private GpioPinValue ledPinValue;
+        private GpioPinValue ledPinValue = GpioPinValue.High;
         private SolidColorBrush redBrush = new SolidColorBrush(Windows.UI.Colors.Red);
         private SolidColorBrush grayBrush = new SolidColorBrush(Windows.UI.Colors.LightGray);
     }
