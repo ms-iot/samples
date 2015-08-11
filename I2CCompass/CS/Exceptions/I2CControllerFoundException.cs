c0da3a418776fd64075cf8c00ed964e2f7b20b1c
reneherrero <reneherrero@gmail.com>
2015-08-10 21:07:21 -0500

    Added the I2C Compass sample.

---
+++ I2CCompass/CS/Exceptions/I2CControllerFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace I2CCompass
+{
+    public class I2CControllerFoundException : Exception
+    {
+        public I2CControllerFoundException()
+            : base("Could not find the I2C controller")
+        { }
+    }
+}
---
+++ I2CCompass/CS/Sensors/ICompass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace I2CCompass.Sensors
+{
+    public interface ICompass : IDisposable
+    {
+        event EventHandler<CompassReading> CompassReadingChangedEvent;
+
+        bool IsInitialized { get; }
+
+        MagnetometerMeasurementMode MeasurementMode { get; }
+
+        Task SetModeAsync(MagnetometerMeasurementMode measurementMode);
+    }
+}
