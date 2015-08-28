a0e1b9b52ac904d78d1bc7fd2acea10ac9aa2026
Jordan Rhee <jordanrh@microsoft.com>
2015-08-27 19:39:38 -0700

    Initial working version of OneWire demo.

---
+++ GpioOneWire/MainPage.xaml.cpp
@@ -0,0 +1,237 @@
+﻿//
+// MainPage.xaml.cpp
+// Implementation of the MainPage class.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+using namespace GpioOneWire;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+using namespace Windows::UI::Core;
+using namespace Windows::System::Threading;
+using namespace Windows::Devices::Gpio;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+void GpioOneWire::Dht11::Init (GpioPin^ Pin)
+{
+    // Use InputPullUp if supported, otherwise fall back to Input (floating)
+    this->inputDriveMode =
+        Pin->IsDriveModeSupported(GpioPinDriveMode::InputPullUp) ?
+        GpioPinDriveMode::InputPullUp : GpioPinDriveMode::Input;
+
+    Pin->SetDriveMode(this->inputDriveMode);
+    this->pin = Pin;
+}
+
+_Use_decl_annotations_
+HRESULT GpioOneWire::Dht11::Sample (GpioOneWire::Dht11Reading& Reading)
+{
+    Reading = Dht11Reading();
+
+    LARGE_INTEGER qpf;
+    QueryPerformanceFrequency(&qpf);
+
+    // convert microseconds to QPF units
+    const unsigned int oneThreshold = static_cast<unsigned int>(
+        110LL * qpf.QuadPart / 1000000LL);
+
+    // Set pin as input pull up
+    this->pin->SetDriveMode(this->inputDriveMode);
+
+    // Latch low value onto pin
+    this->pin->Write(GpioPinValue::Low);
+
+    // Set pin as output
+    this->pin->SetDriveMode(GpioPinDriveMode::Output);
+
+    // Wait for at least 18 ms
+    Sleep(18);
+
+    // Set pin back to input
+    this->pin->SetDriveMode(this->inputDriveMode);
+    
+    GpioPinValue previousValue = this->pin->Read();
+    
+    // catch the first rising edge
+    ULONGLONG endTickCount = GetTickCount64() + 1;
+    for (;;) {
+        if (GetTickCount64() > endTickCount) {
+            return HRESULT_FROM_WIN32(ERROR_TIMEOUT);
+        }
+        
+        GpioPinValue value = this->pin->Read();
+        if (value != previousValue) {
+            // rising edgue?
+            if (value == GpioPinValue::High) {
+                break;
+            }
+            previousValue = value;
+        }
+    }
+    
+    LARGE_INTEGER prevTime = { 0 };
+    unsigned int i = 0;
+    
+    endTickCount = GetTickCount64() + 10;
+
+    // capture every transition until end time is reached
+    for (;;) {
+        if (GetTickCount64() > endTickCount) {
+            return HRESULT_FROM_WIN32(ERROR_TIMEOUT);
+        }
+            
+        GpioPinValue value = this->pin->Read();
+        if ((previousValue == GpioPinValue::High) && (value == GpioPinValue::Low)) {
+            LARGE_INTEGER now;
+            QueryPerformanceCounter(&now);
+            
+            if (i != 0) {
+                unsigned int difference = static_cast<unsigned int>(
+                    now.QuadPart - prevTime.QuadPart);
+                Reading.bits[Reading.bits.size() - i] = 
+                    difference > oneThreshold;
+            }
+            
+            prevTime = now;
+            ++i;
+            if (i == (Reading.bits.size() + 1))
+                break;
+        }
+        
+        previousValue = value;
+    }
+    
+    if (i != (Reading.bits.size() + 1)) {
+        // did not receive all bits
+        return HRESULT_FROM_WIN32(ERROR_IO_DEVICE);
+    }
+    
+    if (!Reading.IsValid()) {
+        // checksum mismatch
+        return HRESULT_FROM_WIN32(ERROR_INVALID_DATA);
+    }
+    
+    return S_OK;
+}
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+}
+
+void GpioOneWire::MainPage::Page_Loaded(
+    Platform::Object^ sender,
+    Windows::UI::Xaml::RoutedEventArgs^ e
+    )
+{
+    GpioController^ controller = GpioController::GetDefault();
+    if (!controller) {
+        this->statusText->Text = L"GPIO is not available on this system";
+        return;
+    }
+
+    GpioPin^ pin;
+    try {
+        pin = controller->OpenPin(18);
+    } catch (Exception^ ex) {
+        this->statusText->Text = L"Failed to open GPIO pin: " + ex->Message;
+        return;
+    }
+
+    this->dht11.Init(pin);
+
+    TimeSpan period = { 2 * 10000000LL };
+    this->timer = ThreadPoolTimer::CreatePeriodicTimer(
+        ref new TimerElapsedHandler(this, &MainPage::timerElapsed),
+        period);
+
+    this->statusText->Text = L"Status: Initialized Successfully";
+}
+
+void GpioOneWire::MainPage::timerElapsed (
+    Windows::System::Threading::ThreadPoolTimer^ Timer
+    )
+{
+    HRESULT sensorHr;
+    Dht11Reading reading;
+
+    int retryCount = 0;
+    do {
+        sensorHr = this->dht11.Sample(reading);
+    } while (FAILED(sensorHr) && (++retryCount < 20));
+
+    this->Dispatcher->RunAsync(
+        CoreDispatcherPriority::Normal,
+        ref new DispatchedHandler([this, reading, sensorHr, retryCount] ()
+    {
+        if (FAILED(sensorHr)) {
+            this->humidityText->Text = L"Humidity: (failed)";
+            this->temperatureText->Text = L"Temperature: (failed)";
+            
+            switch (sensorHr) {
+            case __HRESULT_FROM_WIN32(ERROR_IO_DEVICE):
+                this->statusText->Text = L"Did not catch all falling edges";
+                break;
+            case __HRESULT_FROM_WIN32(ERROR_TIMEOUT):
+                this->statusText->Text = L"Timed out waiting for sample";
+                break;
+            case __HRESULT_FROM_WIN32(ERROR_INVALID_DATA):
+                this->statusText->Text = L"Checksum validation failed";
+                break;
+            default:
+                this->statusText->Text = L"Failed to get reading";
+            }
+
+            return;
+        }
+
+        HRESULT hr;
+        wchar_t buf[128];
+
+        hr = StringCchPrintfW(
+            buf,
+            ARRAYSIZE(buf),
+            L"Humidity: %f",
+            reading.Humidity());
+        if (FAILED(hr)) {
+            throw ref new Exception(hr, L"Failed to print string");
+        }
+
+        this->humidityText->Text = ref new String(buf);
+        
+        hr = StringCchPrintfW(
+            buf,
+            ARRAYSIZE(buf),
+            L"Temperature: %f",
+            reading.Temperature());
+        if (FAILED(hr)) {
+            throw ref new Exception(hr, L"Failed to print string");
+        }
+
+        this->temperatureText->Text = ref new String(buf);
+
+        hr = StringCchPrintfW(
+            buf,
+            ARRAYSIZE(buf),
+            L"Succeeded (%d %s)",
+            retryCount,
+            (retryCount == 1) ? L"retry" : L"retries");
+        if (FAILED(hr)) {
+            throw ref new Exception(hr, L"Failed to print string");
+        }
+
+        this->statusText->Text = ref new String(buf);
+    }));
+}
---
+++ GpioOneWire/MainPage.xaml.h
@@ -0,0 +1,73 @@
+﻿//
+// MainPage.xaml.h
+// Declaration of the MainPage class.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace GpioOneWire
+{
+    struct Dht11Reading {
+        bool IsValid ( ) const
+        {
+            unsigned long long value = this->bits.to_ullong();
+            unsigned int checksum = 
+                ((value >> 32) & 0xff) +
+                ((value >> 24) & 0xff) +
+                ((value >> 16) & 0xff) +
+                ((value >> 8) & 0xff);
+            
+            return (checksum & 0xff) == (value & 0xff);
+        }
+    
+        double Humidity ( ) const
+        {
+            unsigned long long value = this->bits.to_ullong();
+            return ((value >> 32) & 0xff) + ((value >> 24) & 0xff) / 10.0;
+        }
+    
+        double Temperature ( ) const
+        {
+            unsigned long long value = this->bits.to_ullong();
+            return ((value >> 16) & 0xff) + ((value >> 8) & 0xff) / 10.0;
+        }
+
+        std::bitset<40> bits;
+    };
+
+    class Dht11
+    {
+    public:
+        
+        Dht11 ( ) : 
+            pin(nullptr),
+            inputDriveMode(Windows::Devices::Gpio::GpioPinDriveMode::Input)
+        { }
+
+        void Init (Windows::Devices::Gpio::GpioPin^ Pin);
+
+        HRESULT Sample (_Out_ Dht11Reading& Reading);
+
+    private:
+        Windows::Devices::Gpio::GpioPin^ pin;
+        Windows::Devices::Gpio::GpioPinDriveMode inputDriveMode;
+    };
+
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public ref class MainPage sealed
+	{
+	public:
+		MainPage();
+
+    private:
+        void Page_Loaded(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+        void timerElapsed (Windows::System::Threading::ThreadPoolTimer^ Timer);
+
+        Dht11 dht11;
+        Windows::System::Threading::ThreadPoolTimer^ timer;
+    };
+}
