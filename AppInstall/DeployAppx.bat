9c3136466d8d22a28fe4f07d67dad4e8b5f69ece
makal <makal@microsoft.com>
2015-12-04 15:50:57 -0800

    Fixing script to reflect x86 name

--- AppInstall/DeployAppx.bat
+++ AppInstall/DeployAppx.bat
@@ -15,8 +15,8 @@ SETLOCAL
 :: set dependencylist=depappx1name depappx2name (Name only. No need for .appx extension. You can delimit mutliple dependency appxs with a space.)
 :: ---------------------------------------------------------------------
 set defaultappx=MainAppx_1.0.0.0_x86
-set defaultappxid=bfa3eb48-79d5-4245-9bfe-6d2ffeef846d_q8jky9dv1tcdg!App
-set dependencylist=Microsoft.VCLibs.x86.14.00
+set defaultappxid=7a11c6a0-d9a8-40cd-80cf-91d6a409edae_pft3qchy8afw0
+set dependencylist=Microsoft.VCLibs.x86.14.00 Microsoft.NET.Native.Runtime.1.1 Microsoft.NET.Native.Framework.1.2
 ::set defaultappx=MainAppx_1.0.2.0_x86
 
 ::
