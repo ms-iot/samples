ca14460a522202813653fdb3a843e5be617f22ae
trmck <trmck@microsoft.com>
2016-04-28 21:29:58 -0700

    Added HTML UI and functionality for connecting to WiFi network

--- WiFiConnect/Node/server.js
+++ WiFiConnect/Node/server.js
@@ -1,43 +1,92 @@
 var http = require('http'),
+    fs = require('fs'),
+    url = require('url'),
     uwp = require('uwp');
 uwp.projectNamespace('Windows');
 
 var firstAdapter = null;
 var resultCollection = [];
+var selectedNetwork = null;
 
-function status(message){
-    console.log(message);
+var _status = null;
+var _errors = [];
+
+var urlMap = {
+    '/': function (req, res){
+        fs.readFile('./default.html', function (err, html) {
+            if (err)
+                throw err;
+            res.simpleHTML(200, html);
+        });
+    },
+    '/listener/state': function (req, res){
+        res.simpleJSON(200, { adapter: firstAdapter, results: resultCollection, status: _status, errors: _errors });
+        _errors = [];
+    },
+    '/listener/scan': function (req, res){
+        scanForNetworks();
+        res.simpleJSON(202, null);
+    },
+    '/listener/connect': function (req, res){
+        var url_parts = url.parse(req.url, true);
+        var query = url_parts.query;
+        var key = (url_parts.key != undefined) ? url_parts.key : null;
+        connectToNetwork(query.id, key);
+        res.simpleJSON(202, null);
+    },
+    '/listener/disconnect': function (req, res) {
+        var url_parts = url.parse(req.url, true);
+        var query = url_parts.query;
+        connectToNetwork(query.id);
+        res.simpleJSON(202, null);
+    }
+};
+
+function status(message, isError){
+    _status = message;
+    if (isError) {
+        console.error(message);
+        _errors.push(message);
+    }
+    else {
+        console.log(message);
+    }
+}
+
+function notFound(req, res) {
+    var NOT_FOUND = "Not Found\n";
+    res.writeHead(404, { "Content-Type": "text/plain", "Content-Length": NOT_FOUND.length });
+    res.write(NOT_FOUND);
+    res.end();
 }
 
 (function () {
     var access = Windows.Devices.WiFi.WiFiAdapter.requestAccessAsync()
-    .then(function (accessResult){
+    .then(function (accessResult) {
         if (accessResult != Windows.Devices.WiFi.WiFiAccessStatus.allowed) {
-            status('Access to WiFi control has been denied!');
+            status('Access to WiFi control has been denied!', true);
         }
         else {
             Windows.Devices.WiFi.WiFiAdapter.findAllAdaptersAsync()
             .done(function (deviceResults) {
-                if (deviceResults.length > 1) {
-                    Windows.Devices.WiFi.WiFiAdapter.fromIdAsync(deviceResults[0].id).done(function (adapter) {
-                        firstAdapter = adapter;
-                    });
+                if (deviceResults.length > 0) {
+                    firstAdapter = deviceResults[0];
                 }
                 else {
-                    status('No WiFi Adapters detected on this device.');
+                    status('No WiFi Adapters detected on this device.', true);
                 }
             });
         }
-    })
-
+    });
 })();
 
 function scanForNetworks(){
-    firstAdapter.scanAsync().done(function () {
-        console.log('Scan complete');
-        
+    firstAdapter.scanAsync().done(function () {       
         var report = firstAdapter.networkReport;
         resultCollection = [];
+        
+        if (_status == null)
+            status('Scan completed.', false);
 
         for (i = 0; i < report.availableNetworks.length; i++) {
             var network = report.availableNetworks[i];
@@ -46,25 +95,58 @@ function scanForNetworks(){
     });
 }
 
-function connectToNetwork(){
-    var selectedNetwork;
+function connectToNetwork(index, key){
+    if (resultCollection.length <= index) {
+        status('Invalid network selected', true);
+        return;
+    }
+
+    selectedNetwork = resultCollection[index];
     var reconnectionKind = Windows.Devices.WiFi.WiFiReconnectionKind.manual;
 
-    if (selectedNetwork.availableNetwork.SecuritySettings.networkAuthenticationType == Windows.Networking.Connectivity.NetworkAuthenticationType.open80211) {
-        firstAdapter.connectAsync(selectedNetwork.availableNetwork, reconnectionKind);
+    if (selectedNetwork.availableNetwork.securitySettings.networkAuthenticationType == Windows.Networking.Connectivity.NetworkAuthenticationType.open80211) {
+        firstAdapter.connectAsync(selectedNetwork.availableNetwork, reconnectionKind).done(onAdapterConnectionComplete);
     }
     else {
-        var credential = new PasswordCredential();
-        credential.password = NetworkKey.password;
+        var credential = new Windows.Security.Credentials.PasswordCredential();
+        credential.password = key;
 
-        firstAdapter.connectAsync(selectedNetwork.availableNetwork, reconnectionKind, credential);
+        firstAdapter.connectAsync(selectedNetwork.availableNetwork, reconnectionKind, credential).done(onAdapterConnectionComplete);
+    }    
+}
+
+function onAdapterConnectionComplete(result){
+    scanForNetworks();
+
+    var connectionStatus;
+    switch (result.connectionStatus) {
+        case 1:
+            connectionStatus = 'Success';
+            break;
+        case 2:
+            connectionStatus = 'Access Revoked';
+            break;
+        case 3:
+            connectionStatus = 'Invalid Credential';
+            break;
+        case 4:
+            connectionStatus = 'Network Not Available';
+            break;
+        case 5:
+            connectionStatus = 'Timeout';
+            break;
+        case 6:
+            connectionStatus = 'Unsupported Authentication Protocol';
+            break;
+        default:
+            connectionStatus = 'Unspecified Failure';
     }
 
-    if (result.connectionStatus = Windows.Devices.WiFi.WiFiConnectionStatus.success) {
-        status('Successfully connected to ' + selectedNetwork.ssid);
+    if (result.connectionStatus == Windows.Devices.WiFi.WiFiConnectionStatus.success) {
+        status('Successfully connected to ' + selectedNetwork.ssid, false);
     }
     else {
-        status('Could not connect to ' + selectedNetwork.ssid + '. Error: ' + result.connectionStatus);
+        status('Could not connect to ' + selectedNetwork.ssid + '. Error: ' + connectionStatus, true);
     }
 }
 
@@ -72,47 +154,59 @@ function WiFiNetworkDisplay(availableNetwork, adapter){
     this.availableNetwork = availableNetwork;
     this.adapter = adapter;
     this.connectivityLevel = 'Not Connected';
+    this.ssid = this.availableNetwork.ssid;
+    this.bssid = this.availableNetwork.bssid;
+    this.channelCenterFrequency = this.availableNetwork.channelCenterFrequencyInKilohertz + 'kHz';
+    this.rssi = this.availableNetwork.networkRssiInDecibelMilliwatts + 'dBm';
+    this.authenticationType = this.availableNetwork.securitySettings.networkAuthenticationType;
+    this.encryptionType = this.availableNetwork.securitySettings.networkEncryptionType;
+    this.securitySettings = 'Authentication: ' + this.availableNetwork.securitySettings.networkAuthenticationType + '; Encryption: ' + this.availableNetwork.securitySettings.networkEncryptionType;
     
-    function updateConnectivityLevel() {
+    var self = this;
+    this.updateConnectivityLevel = function() {
         var connectedSsid = null;
 
-        this.adapter.NetworkAdapter.getConnectedProfileAsync().done(function (profile) {
+        this.adapter.networkAdapter.getConnectedProfileAsync().done(function (profile) {
             if (profile != null && profile.isWlanConnectionProfile && profile.wlanConnectionProfileDetails != null) {
-                connectedSsid = profile.WlanConnectionProfileDetails.getConnectedSsid();
+                connectedSsid = profile.wlanConnectionProfileDetails.getConnectedSsid();
             }
 
-            if (connectedSsid != null && _connectedSsid.length > 0) {
-                if (connectedSsid == this.availableNetwork.ssid) {
-                    this.connectivityLevel = profile.getNetworkConnectivityLevel();
+            if (connectedSsid != null && connectedSsid.length > 0) {
+                if (connectedSsid == self.availableNetwork.ssid) {
+                    if (profile.getNetworkConnectivityLevel() == 0) {
+                        self.connectivityLevel = 'Not Connected';
+                    }
+                    else {
+                        self.connectivityLevel = 'Connected';
+                        status('Connected to ' + self.ssid, false);
+                    }
                 }
             }
         });
     };
 
-    this.ssid = function () {
-        return this.availableNetwork.ssid;
-    };
-
-    this.bssid = function () {
-        return this.availableNetwork.bssid;
-    };
-
-    this.channelCenterFrequency = function () {
-        return this.availableNetwork.channelCenterFrequencyInKilohertz + 'kHz';
-    };
-    
-    this.rssi = function () {
-        return this.availableNetwork.networkRssiInDecibelMilliwatts + 'dBm';
-    };
-    
-    this.securitySettings = function () {
-        return 'Authentication: ' + this.availableNetwork.SecuritySettings.networkAuthenticationType + '; Encryption: ' + this.availableNetwork.SecuritySettings.networkEncryptionType;
-    };
+    this.updateConnectivityLevel();
 }
 
-/*
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(1337);
- */
+    handler = urlMap[url.parse(req.url).pathname] || notFound;
+    
+    res.simpleHTML = function (code, body) {
+        res.writeHead(code, {
+            "Content-Type": "text/html",
+            "Content-Length": body.length
+        });
+        res.end(body);
+    };
+    
+    res.simpleJSON = function (code, obj) {
+        var body = JSON.stringify(obj);
+        res.writeHead(code, {
+            "Content-Type": "application/json",
+            "Content-Length": body.length
+        });
+        res.end(body);
+    };
+    
+    handler(req, res);
+}).listen(80);
