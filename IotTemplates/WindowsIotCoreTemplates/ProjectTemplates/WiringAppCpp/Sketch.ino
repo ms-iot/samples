fd8a4e6c00bffc80830dbb69793181496c311cde
Mahmoud Saleh <msaleh@microsoft.com>
2016-05-12 12:18:19 -0700

    Update Nuget package for Arduino Wiring
    Replace the source-based Nuget package with binary ones.

--- IotTemplates/WindowsIotCoreTemplates/ProjectTemplates/WiringAppCpp/Sketch.ino
+++ IotTemplates/WindowsIotCoreTemplates/ProjectTemplates/WiringAppCpp/Sketch.ino
@@ -1,20 +1,22 @@
-
 // The Arduino Wiring application template is documented at 
 // http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+
+// Use GPIO pin 5
+const unsigned int LED_PIN = GPIO5;
 
 void setup()
 {
     // put your setup code here, to run once:
 
-    pinMode(GPIO5, OUTPUT);
+    pinMode(LED_PIN, OUTPUT);
 }
 
 void loop()
 {
     // put your main code here, to run repeatedly:
 
-    digitalWrite(GPIO5, LOW);
+    digitalWrite(LED_PIN, LOW);
     delay(500);
-    digitalWrite(GPIO5, HIGH);
+    digitalWrite(LED_PIN, HIGH);
     delay(500);
 }
