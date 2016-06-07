244776a54c12e11fbbbf62205706e3be36946512
Divya Mahadevan <t-dimaha@microsoft.com>
2016-06-07 15:38:11 -0700

    started project

---
+++ IoTSender/IoTSender/AzureIoTHub.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+
+static class AzureIoTHub
+{
+    //
+    // Note: this connection string is specific to the device "dimaha01". To configure other devices,
+    // see information on iothub-explorer at http://aka.ms/iothubgetstartedVSCS
+    //
+    const string deviceConnectionString = "HostName=hub-dimaha.azure-devices.net;DeviceId=dimaha01;SharedAccessKey=Pp+rGghQKmhx4Rdl4OaUGF9dcwHZ+n+Zt3jQwO3xFic=";
+
+    //
+    // To monitor messages sent to device "dimaha01" use iothub-explorer as follows:
+    //    iothub-explorer HostName=hub-dimaha.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=XCKVcZS+dRV6be8cBqveTeJIeO6HyJ9uiIKohaDLjDY= monitor-events "dimaha01"
+    //
+
+    // Refer to http://aka.ms/azure-iot-hub-vs-cs-wiki for more information on Connected Service for Azure IoT Hub
+
+    public static async Task SendDeviceToCloudMessageAsync()
+    {
+        var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Amqp);
+
+#if WINDOWS_UWP
+        var str = "Hello, Cloud from a UWP C# app!";
+#else
+        var str = "Hello, Cloud from a C# app!";
+#endif
+        var message = new Message(Encoding.ASCII.GetBytes(str));
+
+        await deviceClient.SendEventAsync(message);
+    }
+
+    public static async Task<string> ReceiveCloudToDeviceMessageAsync()
+    {
+        var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Amqp);
+
+        while (true)
+        {
+            var receivedMessage = await deviceClient.ReceiveAsync();
+
+            if (receivedMessage != null)
+            {
+                var messageData = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+                await deviceClient.CompleteAsync(receivedMessage);
+                return messageData;
+            }
+
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+    }
+}
