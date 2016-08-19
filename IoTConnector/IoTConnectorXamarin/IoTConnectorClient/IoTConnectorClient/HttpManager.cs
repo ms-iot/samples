41e267a3954e4c3f417200683afe42d0759ec39c
Divya Mahadevan <t-dimaha@microsoft.com>
2016-08-19 13:38:48 -0700

    added xamarin project to connector app

---
+++ IoTConnector/IoTConnectorXamarin/IoTConnectorClient/IoTConnectorClient/HttpManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+
+namespace IoTConnectorClient
+{
+    
+    class HttpManager
+    {
+        private HttpInterfaceManager httpInterfaceManager;
+
+        public IAsyncAction Initialize()
+        {
+            return Task.Run(async () => {
+                this.httpInterfaceManager = new HttpInterfaceManager(Constants.PORT);
+                this.httpInterfaceManager.StartServer();
+            }).AsAsyncAction();
+        }
+    }
+}
