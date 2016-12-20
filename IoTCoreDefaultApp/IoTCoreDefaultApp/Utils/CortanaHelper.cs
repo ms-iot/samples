a2797267f7aa3b86d2036b312c35dd9d70fc1e5b
jslobodzian <joslobo@microsoft.com>
2016-12-20 15:49:21 -0800

    Add Voice Activation, Cortana Consent and Cortana AboutMe button to IotCoreDefaultApp. (#387)
    * This change encapsulates UI changes to the IoTCoreDefaultApp for providing
    This change encapsulates UI changes to the IoTCoreDefaultApp for providingConsent and Voice Activation to Cortana.  The change:
      - integrates with the latest API's available in the latest standalone SDK.
      - removes a temporary button from the BASIC settings page
      - adds a dedicated Cortana settings page to to the IotCoreDefaultApp
      - adds a "Voice Activation" toggle switch that will launch Cortana to the Consent page if Consent has not been granted
      - provides a mechansim for launching the AboutMe settings page within Cortana
      - launches Cortana as part of the OOBE experience
    
    Lines starting
    
    * Address Code Review
    
    * Removed unnecesary using statement.

---
+++ IoTCoreDefaultApp/IoTCoreDefaultApp/Utils/CortanaHelper.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Threading.Tasks;
+using Windows.Foundation;
+#if BUILDWITHCORTANA
+using Windows.Services.Cortana;
+#endif
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace IoTCoreDefaultApp
+{
+    class CortanaHelper
+    {
+        public static void LaunchCortanaToConsentPageAsyncIfNeeded()
+        {
+#if BUILDWITHCORTANA
+            // Do nothing for devices that do not support Cortana
+            if (CortanaSettings.IsSupported())
+            {
+                // Ordinarily, this is run during a first use Out-of-box-Experience (OOBE) and voice consent will NOT be granted.
+                // So, we launch Cortana to it's Consent Page as part of OOBE to give the end-user an opportunity to give consent.
+                // However on development systems where the IotCoreDefaultApp may be deployed repeatedly after Cortana consent has
+                // already been granted we will bypass the Cortana Launch.
+                var cortanaSettings = CortanaSettings.GetDefault();
+                if (!cortanaSettings.HasUserConsentToVoiceActivation)
+                {
+                    LaunchCortanaToConsentPageAsync();
+                }
+            }
+#endif
+        }
+
+        public static Task<bool> LaunchCortanaToConsentPageAsync()
+        {
+            // NOTE: When integrating this sample code into your specific device, Microsoft recommends changing the QuerySourceSecondaryId in the URI to "IoT_<MANUFACTURER>_<DEVICE_DESCRIPTION>"
+            var uri = new Uri("ms-cortana://CapabilitiesPrompt/?RequestedCapabilities=InputPersonalization,Microphone,Personalization&QuerySourceSecondaryId=IoT&QuerySource=Microphone&DismissAfterConsent=True");
+            return Windows.System.Launcher.LaunchUriAsync(uri).AsTask<bool>();
+        }
+
+        public static Task<bool> LaunchCortanaToAboutMeAsync()
+        {
+            var uri = new Uri("ms-cortana://AboutMe");
+            return Windows.System.Launcher.LaunchUriAsync(uri).AsTask<bool>();
+        }
+    }
+}
