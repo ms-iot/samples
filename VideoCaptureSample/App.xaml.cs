a51b2d1eef6bad90938d001239751c0ccdc5a85e
Artem Zhurid <artemz@microsoft.com>
2017-03-24 12:01:48 -0700

    Remove AppInsight from VideoCaptureSample

--- VideoCaptureSample/App.xaml.cs
+++ VideoCaptureSample/App.xaml.cs
@@ -19,9 +19,6 @@ namespace VideoCaptureSample
          /// </summary>
         public App()
         {
-            Microsoft.ApplicationInsights.WindowsAppInitializer.InitializeAsync(
-                Microsoft.ApplicationInsights.WindowsCollectors.Metadata |
-                Microsoft.ApplicationInsights.WindowsCollectors.Session);
             this.InitializeComponent();
         }
 
