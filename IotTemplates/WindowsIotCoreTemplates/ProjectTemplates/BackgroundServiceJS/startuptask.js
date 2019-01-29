44ab6acef96bf87abb550cbb62e843312deeb9ea
Rachit Bajpai <rachitb@microsoft.com>
2019-01-28 16:23:14 -0800

    Removing BackgroundServiceJS template

--- IotTemplates/WindowsIotCoreTemplates/ProjectTemplates/BackgroundServiceJS/startuptask.js
+++
@@ -1,7 +0,0 @@
-// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
-(function () {
-    "use strict";
-
-    // TODO: Insert code here for the startup task
-    
-})();
